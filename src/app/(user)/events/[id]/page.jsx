176dc6afbc4c3859ed8c3b95915c175695d85610
WaiLynn4 <wlay21416@gmail.com>
2025-07-14 16:23:44 +0630

    new event by wai

--- src/app/(user)/events/[id]/page.jsx
+++ src/app/(user)/events/[id]/page.jsx
@@ -1,30 +1,9 @@
-// "use client";
-
-// import { Box, Typography } from "@mui/material";
-// import { useParams } from "next/navigation";
-
-// //Dynamic Route Page
-// //Params : id
-
-
-// export default function EventDetail() {
-//     const params = useParams();
-//     const id = params.id
-//     console.log('EventID: ', params.id);
-    
-//     return(
-//         <Box>
-//             <Typography>EventDetail:{id}</Typography>
-//         </Box>
-//     );
-
-// }
-
 "use client";
 
 import { Box, FormControl, Grid, MenuItem, Select, Typography } from "@mui/material";
 import Image from "next/image";
 import { useParams } from "next/navigation";
+import { useState } from "react";
 
 const categoryData = {
   1: "Engagement",
@@ -38,9 +17,38 @@ const categoryData = {
   9: "Art Gallery",
 };
 
+const categoryImages = {
+  1: "/assets/w6.png",
+  2: "/assets/b5.png",
+  3: "/assets/birthday.png",
+  4: "/assets/party-hall.jpg",
+  5: "/assets/party.png",
+  6: "/assets/room.jpg",
+  7: "/assets/web-indoor.jpg",
+  8: "/assets/birdal.png",
+  9: "/assets/Why-kairo.jpg",
+};
+
+const selectOptions = {
+  1: ["Ballroom", "Garden Venue", "Rooft Top"],
+  2: ["Ballroom", "Outdoor"],
+  3: ["Private Room", "Ballroom", "Outdoor"],
+  4: ["Private Room", "Outdoor"],
+  5: ["Private Room", "Outdoor"],
+  6: ["Private Room", "Rooft Top"],
+  7: ["Private Room", "Ballroom", "Outdoor"],
+  8: ["Private Room"],
+  9: ["Indoor Space"],
+};
+
+
+
+
 export default function CategoryPage() {
   const params = useParams();
   const id = params.id;
+  const [selectedOption, setSelectedOption] = useState("");
+
 
   return (
     <Box sx={{ p: 4 }}>
@@ -57,8 +65,43 @@ export default function CategoryPage() {
               
             />
             
+            
+          </Box>
         </Grid>
-      
+
+        {/* right grid */}
+        <Grid item xs={12} md={6} >
+          <Box position="relative" mb={30} ml={25} >
+            <Typography sx={{ fontSize: 40, fontWeight: "bold" ,ml: 25}}>
+            {categoryData[id]}
+          </Typography>
+          <Typography sx={{ fontSize: 30, mt: 2 , fontWeight: "bold"}}>
+            Your Dream {categoryData[id]}, Perfectly Planned
+          </Typography>
+          <Typography sx={{ fontSize: 30, mt: 2, ml: 18, fontWeight:"bold" }}>
+            Choose {categoryData[id]} Venues
+          </Typography>
+          <FormControl fullWidth sx={{ mt: 4, width: 300 }}>
+ 
+  <Select
+    value={selectedOption}
+    label="Select Option"
+    onChange={(e) => setSelectedOption(e.target.value)}
+  >
+    {(selectOptions[id] || []).map((option, idx) => (
+      <MenuItem key={idx} value={option}>
+        {option}
+      </MenuItem>
+    ))}
+  </Select>
+</FormControl>
+
+        
+
+          </Box>
+          
+        </Grid>
+      </Grid>
     </Box>
   );
-}
+} 
